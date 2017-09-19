b7938dfd3127541d72a53222d23b140d9c56aa39
Vinaya Bhattarai <vnbhattarai@protonmail.ch>
2017-09-19 12:01:49 +0300

    feat: ✨ 05 preact stateful components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
-import { h } from "preact";
+import { h, Component } from "preact";
+import User from "./User";
 
-export function App() {
-  return (
-    <div class="app">
-      <h1>Hello world!</h1>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null,
+      loading: true
+    };
+  }
+  componentDidMount() {
+    fetch(this.props.config.urls.user)
+      .then(resp => resp.json())
+      .then(user => {
+        this.setState({
+          user,
+          loading: false
+        });
+      })
+      .catch(err => console.error(err));
+  }
+  render() {
+    return (
+      <div class="app">
+        {this.state.loading ? (
+          <p>Please wait...</p>
+        ) : (
+          <User image={this.state.user.avatar_url} name={this.state.user.name} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import { h, render } from "preact";
 import App from "./components/App";
 
-render(<App />, document.getElementById("app"));
+const config = {
+  urls: {
+    user: "https://api.github.com/users/vnbhattarai"
+  }
+};
+
+render(<App config={config} />, document.getElementById("app"));
