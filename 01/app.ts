dfaae6090eaa3e0c7736d9653036a36279dee7ae
Vinaya Bhattarai <vnbhattarai@protonmail.ch>
2017-09-24 01:20:53 +0300

    feat: ✨ up and running

---
+++ 01/app.ts
@@ -0,0 +1,5 @@
+class Person {
+  name = "roger";
+}
+
+// tsc --watch --out bundle.js app.ts
---
+++ 01/bundle.js
@@ -0,0 +1,7 @@
+var Person = /** @class */ (function () {
+    function Person() {
+        this.name = "roger";
+    }
+    return Person;
+}());
+// tsc --watch --out bundle.js app.ts
