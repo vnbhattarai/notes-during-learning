72053a22b2b6a2bb28e477024069d82d06047246
Vinaya Bhattarai <vnbhattarai@protonmail.ch>
2017-09-24 01:42:14 +0300

    feat: ✨ Config using tsconfig
    This lesson includes rootDir, noEmitOnError and OutDir config options

--- 01/app.ts
+++
@@ -1,5 +0,0 @@
-class Person {
-  name = "roger";
-}
-
-// tsc --watch --out bundle.js app.ts
--- 01/bundle.js
+++
@@ -1,7 +0,0 @@
-var Person = /** @class */ (function () {
-    function Person() {
-        this.name = "roger";
-    }
-    return Person;
-}());
-// tsc --watch --out bundle.js app.ts
