314236e861f5f25b19c071b6a7fce1f93249c682
Vinaya Bhattarai <vnbhattarai@protonmail.ch>
2017-09-24 02:18:08 +0300

    feat: ✨ Use system.js to load in browser

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,11 @@
 "use strict";
-var Main = /** @class */ (function () {
-    function Main() {
+Object.defineProperty(exports, "__esModule", { value: true });
+var two_1 = require("./two");
+new two_1.Two();
+var App = /** @class */ (function () {
+    function App() {
+        console.log("Hello hello");
     }
-    return Main;
+    return App;
 }());
+new App();
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { Two } from "./two";
+
+new Two();
+class App {
+  constructor() {
+    console.log("Hello hello");
+  }
+}
+
+new App();
